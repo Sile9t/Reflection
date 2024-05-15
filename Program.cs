101e14645ff9847245dae6eea4646aff2cf049e2
Sile9t <eyesless1jack@gmail.com>
2024-05-15 09:51:09 +0300

    Added files

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Reflection
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    class CustomNameAttribute : Attribute { }
+    class TargetClass
+    {
+        public int _i { get; set; }
+        public string _s { get; set; }
+        private decimal _d { get; set; }
+        public char[] _c { get; set; }
+        public TargetClass(int i, string s, decimal d, char[] c)
+        {
+            _i = i;
+            _s = s;
+            _d = d;
+            _c = c;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
